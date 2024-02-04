48412ecbc94d208c1f4d1b04b26d1ad0f38324be
tomekgladki <tk.gladki467@gmail.com>
2024-02-04 16:33:09 +0100

    evolve again

--- main_code_cat.py
+++ main_code_cat.py
@@ -61,34 +61,39 @@ class Cat:
     def evolve_n_steps(self, steps):
         ht,hx,hy,Du,Dv=list(self.params["par"].values())[0:5]
         for key in self.matrices:
-            self.results_[key]=np.zeros((steps, self.matrices[key].shape()[0], self.matrices[key].shape()[1]))
+            self.results_[key]=np.zeros((steps, self.matrices[key].shape[0], self.matrices[key].shape[1]))
             self.results_[key][0,:,:]=self.matrices[key]
             results=self.results_[key]
-            self.v_results_[key]=np.zeros((steps, self.matrices[key].shape()[0], self.matrices[key].shape()[1]))
+            self.v_results_[key]=np.zeros((steps, self.matrices[key].shape[0], self.matrices[key].shape[1]))
             self.v_results_[key][0,:,:]=self.v_matrices[key]
             v_results=self.v_results_[key]
-            for i in range(steps):
+            for i in range(steps-1): 
+                if key not in ["head","body","tail","LFL", "LBL", "RFL", "RBL"]:
+                    continue
+                else:
                 # matrix u
-                self.results_[key][i+1,1:-1:,1:-1]=results[i,1:-1,1:-1]+Du*ht/(hx**2)*(results[i,2:,1:-1]+results[i,:-2,1:-1]-2*results[i,1:-1,1:-1])+Du*ht/(hy**2)*(results[i,1:-1,2:]+results[i,1:-1,:-2]-2*results[i,1:-1,1:-1])+self.params["func"]["f"](
-                    results[i,1:-1,1:-1],v_results[i,1:-1,1:-1],self.params["par"]
-                )
-                self.results_[key][i+1,0,:]=self.results_[key][i,1,:]
-                self.results_[key][i+1,-1,:]=self.results_[key][i,-2,:]
-                self.results_[key][i+1,:,0]=self.results_[key][i,:,1]
-                self.results_[key][i+1,:,:-1]=self.results_[key][i,:,-2]
-                
-                
-                # matrix v
-                self.v_results_[key][i+1,1:-1,1:-1]=v_results[i,1:-1,1:-1]+Dv*ht/(hx**2)*(v_results[i,2:,1:-1]+v_results[i,:-2,1:-1]-2*v_results[i,1:-1,1:-1])+Du*ht/(hy**2)*(v_results[i,1:-1,2:]+v_results[i,1:-1,:-2]-2*v_results[i,1:-1,1:-1])+self.params["func"]["g"](
-                    results[i,1:-1,1:-1],v_results[i,1:-1,1:-1],self.params["par"]
-                )
-                self.v_results_[key][i+1,0,:]=self.v_results_[key][i,1,:]
-                self.v_results_[key][i+1-1,:]=self.v_results_[key][i,-2,:]
-                self.v_results_[key][i+1:,0]=self.v_results_[key][i,:,1]
-                self.v_results_[key][i+1:,:-1]=self.v_results_[key][i,:,-2]
+                    self.results_[key][i+1,1:-1:,1:-1]=results[i,1:-1,1:-1]+Du*ht/(hx**2)*(results[i,2:,1:-1]+results[i,:-2,1:-1]-2*results[i,1:-1,1:-1])+Du*ht/(hy**2)*(results[i,1:-1,2:]+results[i,1:-1,:-2]-2*results[i,1:-1,1:-1])+self.params["func"]["f"](
+                        results[i,1:-1,1:-1],v_results[i,1:-1,1:-1],self.params["par"]
+                    )
+                    self.results_[key][i+1,0,:]=self.results_[key][i,1,:]
+                    self.results_[key][i+1,-1,:]=self.results_[key][i,-2,:]
+                    self.results_[key][i+1,:,0]=self.results_[key][i,:,1]
+                    self.results_[key][i+1,:,-1]=self.results_[key][i,:,-2]
+                    
+                    
+                    # matrix v
+                    self.v_results_[key][i+1,1:-1,1:-1]=v_results[i,1:-1,1:-1]+Dv*ht/(hx**2)*(v_results[i,2:,1:-1]+v_results[i,:-2,1:-1]-2*v_results[i,1:-1,1:-1])+Du*ht/(hy**2)*(v_results[i,1:-1,2:]+v_results[i,1:-1,:-2]-2*v_results[i,1:-1,1:-1])+self.params["func"]["g"](
+                        results[i,1:-1,1:-1],v_results[i,1:-1,1:-1],self.params["par"]
+                    )
+                    self.v_results_[key][i+1,0,:]=self.v_results_[key][i,1,:]
+                    self.v_results_[key][i+1,-1,:]=self.v_results_[key][i,-2,:]
+                    self.v_results_[key][i+1,:,0]=self.v_results_[key][i,:,1]
+                    self.v_results_[key][i+1,:,-1]=self.v_results_[key][i,:,-2]
 
-                results=self.results_[key]
-                v_results=self.v_results_[key]
+                    results=self.results_[key]
+                    v_results=self.v_results_[key]
+
+                
 
         
 
@@ -107,12 +112,12 @@ params={"ind_lim":{
     "LBL": [(60,0), (60,30), (70,0), (70,30), lambda x: np.random.random(x.shape)],
     "RFL": [(20,70), (20,100), (30,70), (30,100), lambda x: np.random.random(x.shape)],
     "RBL": [(60,70), (60,100), (70, 70), (70,100), lambda x: np.random.random(x.shape)],
-    "I": [(0,0), (0,40), (20,0), (20, 40), lambda x: 0],
-    "II": [(0,60), (0,100), (20,60), (20,100), lambda x: 0],
-    "III": [(30,0), (30,30), (60,0), (60,30), lambda x: 0],
-    "IV": [(30,70), (30,100), (60,70), (60,100), lambda x: 0],
-    "V": [(70,0), (70,45), (100,0), (100,45), lambda x: 0],
-    "VI": [(70,55), (70,100), (100,55), (100,100), lambda x: 0]
+    "I": [(0,0), (0,40), (20,0), (20, 40), lambda x: np.zeros(x.shape)],
+    "II": [(0,60), (0,100), (20,60), (20,100), lambda x: np.zeros(x.shape)],
+    "III": [(30,0), (30,30), (60,0), (60,30), lambda x: np.zeros(x.shape)],
+    "IV": [(30,70), (30,100), (60,70), (60,100), lambda x: np.zeros(x.shape)],
+    "V": [(70,0), (70,45), (100,0), (100,45), lambda x: np.zeros(x.shape)],
+    "VI": [(70,55), (70,100), (100,55), (100,100), lambda x: np.zeros(x.shape)]
     },
         "domain":{
             "grid":np.meshgrid(np.linspace(0,1,100), np.linspace(0,1,100))[0],
@@ -142,3 +147,7 @@ x = Cat(params)
 #plt.show()
 #print(x.matrices["head"])
 x.evolve_n_steps(10)
+plt.imshow(x.results_[-1])
+plt.show()
+#print(x.params["func"]["f"]())
+
