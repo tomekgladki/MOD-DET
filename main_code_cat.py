dd961a3b6df7b5ee551c701953fa29a834535c73
tomekgladki <tk.gladki467@gmail.com>
2024-02-09 03:54:59 +0100

    finish

--- main_code_cat.py
+++ main_code_cat.py
@@ -1,5 +1,6 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.animation as anm
 
 class Cat:
     def __init__(self, params:dict):
@@ -9,6 +10,7 @@ class Cat:
         self.v_matrices={}
         self.results_={} # 3d array
         self.v_results_={} # 3d array
+        self.all_results={} # 3d array
         self.get_matrices()
         self.get_glued_result()
         self.get_v_matrices()
@@ -41,13 +43,8 @@ class Cat:
             else:
                 self.v_matrices[key]=self.glued_result[row_min:row_max, col_min:col_max]
         return self
-    '''
-    def get_arrays(self,matrix1,matrix2):
-        new_matrix = np.array([matrix1, matrix2])
-        return new_matrix
-    '''       
-        
-                
+   
+                       
         
     def get_glued_result(self):
         for key in params["ind_lim"].keys():
@@ -60,7 +57,16 @@ class Cat:
     
     def evolve_n_steps(self, steps):
         ht,hx,hy,Du,Dv=list(self.params["par"].values())[0:5]
+
+        self.all_results=np.zeros((steps, 100, 100))
+        self.all_results[0,:,:]=self.glued_result
         for key in self.matrices:
+
+            row_min=min([tup[0] for tup in params["ind_lim"][key][:-1]])
+            row_max=max([tup[0] for tup in params["ind_lim"][key][:-1]])
+            col_min=min([tup[1] for tup in params["ind_lim"][key][:-1]])
+            col_max=max([tup[1] for tup in params["ind_lim"][key][:-1]])
+
             self.results_[key]=np.zeros((steps, self.matrices[key].shape[0], self.matrices[key].shape[1]))
             self.results_[key][0,:,:]=self.matrices[key]
             results=self.results_[key]
@@ -68,10 +74,11 @@ class Cat:
             self.v_results_[key][0,:,:]=self.v_matrices[key]
             v_results=self.v_results_[key]
             for i in range(steps-1): 
+
                 if key not in ["head","body","tail","LFL", "LBL", "RFL", "RBL"]:
                     continue
                 else:
-                # matrix u
+                    # matrix u
                     self.results_[key][i+1,1:-1:,1:-1]=results[i,1:-1,1:-1]+Du*ht/(hx**2)*(results[i,2:,1:-1]+results[i,:-2,1:-1]-2*results[i,1:-1,1:-1])+Du*ht/(hy**2)*(results[i,1:-1,2:]+results[i,1:-1,:-2]-2*results[i,1:-1,1:-1])+self.params["func"]["f"](
                         results[i,1:-1,1:-1],v_results[i,1:-1,1:-1],self.params["par"]
                     )
@@ -90,8 +97,11 @@ class Cat:
                     self.v_results_[key][i+1,:,0]=self.v_results_[key][i,:,1]
                     self.v_results_[key][i+1,:,-1]=self.v_results_[key][i,:,-2]
 
+                    self.all_results[i+1,row_min:row_max, col_min:col_max]=self.results_[key][i+1,:,:]
                     results=self.results_[key]
                     v_results=self.v_results_[key]
+                    
+
 
                 
 
@@ -100,10 +110,6 @@ class Cat:
         
         return self
 
-    
-
-
-#if __name__=="main":
 params={"ind_lim":{
     "head": [(0,40), (0,60), (20,40), (20,60), lambda x: np.random.random(x.shape)],
     "body": [(20,30), (20,70), (70,30), (70,70), lambda x: np.random.random(x.shape)],
@@ -124,20 +130,21 @@ params={"ind_lim":{
             "dx":1
     },
     "func":{
-        "f": lambda u, v, dict_par: dict_par['r_u']+dict_par['r']*(u**2)/v-dict_par['mi_u']*u,
-        "g": lambda u, v, dict_par: dict_par['r_v']+dict_par['r']*u**2-dict_par['mi_v']*v
+        "f": lambda u, v, dict_par: dict_par['a']+dict_par['b']*(u**2)/(v*(1+dict_par['K']*u**2))-dict_par['c']*u,
+        "g": lambda u, v, dict_par: dict_par['d']*u**2-dict_par['e']*v
     },
     "par":{
-        "ht":1,
+        "ht":0.1,
         "hx":1,
         "hy":1,
-        "Du":0.5,
-        "Dv":0.1,
-        "r":1,
-        "r_u":0.3,
-        "r_v":0.3,
-        "mi_u":0.5,
-        "mi_v":0.5
+        "Du":0.1,
+        "Dv":2.0,
+        "a":0,
+        "b":0.5,
+        "c":0.5,
+        "d":0.5,
+        "e":0.45,
+        "K":0.238
 
     }        
 }
@@ -146,8 +153,24 @@ x = Cat(params)
 #plt.imshow(x.glued_result)
 #plt.show()
 #print(x.matrices["head"])
-x.evolve_n_steps(10)
-plt.imshow(x.results_[-1])
+x.evolve_n_steps(20)
+plt.imshow(x.all_results[-1,:,:])
 plt.show()
+print(np.var(x.all_results[-1,:,:]))
+
+'''
+fig=plt.figure()
+figs=x.all_results[0,:,:]
+def animation(j):
+    figs=x.all_results[j,:,:]
+    return figs
+
+anim=anm.FuncAnimation(fig,
+                      func=animation,
+                      interval = 10,
+                      blit=True)
+plt.show()
+'''                      
 #print(x.params["func"]["f"]())
 
+#print(x.all_results[-1,:,:])
